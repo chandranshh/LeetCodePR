4b811b93f85b7ee9f8a98c2a5f5d7aca5b6d8f01
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-08-13 16:55:39 +0530

    Time: 5 ms (50.84%), Space: 10 MB (14.42%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
- vector<int> shuffle(vector<int>& nums, int n) {
+    vector<int> shuffle(vector<int>& nums, int n) {
+  vector<int>v;
   if (n >= 1 && n <= 500) {
-    auto arr = nums;
-  
     if (nums.size() == 2 * n) {
-  for (int i = 0; i < n; i++) {
-    nums[i * 2] = arr[i];
-    nums[(2 * i) + 1] = arr[n + i];
-}
+      for (int i = 0; i < n; i++) {
+        v.emplace_back(nums[i]);
+        v.emplace_back(nums[n + i]);
+      }
     }
   }
-    return nums;
+  return v;
 }
 };
