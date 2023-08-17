6fd0e9a079e8f4624ba6b07d93bb1c9c79a75925
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-08-17 21:05:05 +0530

    Time: 7 ms (94.65%), Space: 10.7 MB (44.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ump;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+            if (ump.find(target - nums[i]) != ump.end()) {
+                return {ump[target - nums[i]], i};
             }
+            ump[nums[i]] = i;
         }
         return {};
     }
