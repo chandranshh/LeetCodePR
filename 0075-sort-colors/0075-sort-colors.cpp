a908ccc610d90bf1d8d1a0171eaf471bd105486b
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-08-18 15:56:48 +0530

    Time: 0 ms (100%), Space: 8.3 MB (41.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        map<int, int>mp;
+        int count_0=0, count_1=0, count_2=0;
 
         for(int i = 0;i<n;i++){
-            mp[nums[i]]++;
+            if(nums[i]==0){
+                count_0++;
+            }else if(nums[i]==1){
+                count_1++;
+            }
+            else if(nums[i]==2){
+                count_2++;
+            }
         }
         nums.clear();
-        for(auto it : mp){
-            for(int i=0;i<it.second;i++){
-                nums.emplace_back(it.first);
-            }
+        for(int i=0;i<count_0;i++){
+            nums.emplace_back(0);
+        }
+        for(int i=0;i<count_1;i++){
+            nums.emplace_back(1);
+        }
+        for(int i=0;i<count_2;i++){
+            nums.emplace_back(2);
         }
     }
 };
