322030901a3a601797ada4ff9a252765c23ef6d7
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-08-26 21:06:35 +0530

    Time: 50 ms (17.11%), Space: 19.1 MB (44.62%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+	public:
+		vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    if (intervals.empty()) {
+        return {};
+    }
+
+    vector<vector<int>> result;
+    sort(intervals.begin(), intervals.end());
+
+    int start = intervals[0][0];
+    int end = intervals[0][1];
+
+    for (int i = 1; i < intervals.size(); i++) {
+        if (intervals[i][0] <= end) {
+            end = max(end, intervals[i][1]);
+        } else {
+            result.push_back({start, end});
+            start = intervals[i][0];
+            end = intervals[i][1];
+        }
+    }
+
+    result.push_back({start, end});
+    return result;
+}
+};
