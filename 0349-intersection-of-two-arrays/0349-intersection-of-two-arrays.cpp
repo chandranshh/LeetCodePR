b37d64ea3de088862fcd95e621ede471fdea3e91
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-08-27 00:59:20 +0530

    Time: 0 ms (100%), Space: 10.9 MB (7.61%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -8,7 +8,7 @@ set<int> s1(nums1.begin(), nums1.end());
 set<int> s2(nums2.begin(), nums2.end());
 
 for (auto num : s1) {
-    if (s2.count(num)) {
+    if (s2.find(num)!=s2.end()) {
         result.emplace_back(num);
     }
 }
