1e95840a678171a971a1a158d30803621c26ebb8
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-08-27 00:55:48 +0530

    Time: 10 ms (24.52%), Space: 10.9 MB (7.61%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2)    {
+
+        vector<int> result;
+
+set<int> s1(nums1.begin(), nums1.end());
+set<int> s2(nums2.begin(), nums2.end());
+
+for (auto num : s1) {
+    if (s2.count(num)) {
+        result.emplace_back(num);
+    }
+}
+
+return result;
+    }
+};
