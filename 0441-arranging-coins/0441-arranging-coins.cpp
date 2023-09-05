170536bd992e3c015591b46ec05fbdb7c506689a
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2023-09-06 02:44:10 +0530

    Time: 5 ms (50.37%), Space: 5.9 MB (49.31%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int num = n, count = 0, i = 1;
-        
-        do{
-            num -= i;
-            count++;
-        }while(num > i++ );
-        return count;
+    long left = 1;
+    long right = n;
+    long result = 0; 
+
+    while (left <= right) {
+        long mid = left + (right - left) / 2;
+        long totalCoins = (mid * (mid + 1)) / 2;  
+        if (totalCoins == n) {
+            return mid;
+        } else if (totalCoins < n) {
+            left = mid + 1;
+            result = mid; 
+        } else {
+            right = mid - 1;
+        }
     }
+
+    return result;
+}
+
 };
