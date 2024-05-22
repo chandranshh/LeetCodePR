636e46fc7bc22573426e9bb75a38401746ee2df6
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-05-23 02:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (95.49%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int R = m-1;
+
+        long long res = 1;
+
+        for(int i = 1; i <=R;i++){
+            res=res*(N-R+i)/i;
+        }
+        return (int)res;
+    }
+};
