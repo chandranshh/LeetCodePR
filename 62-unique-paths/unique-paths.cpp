3fb9d59c97d1048d43ad8aac7bbc61d20fb70f53
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-05-23 15:33:47 +0530

    Time: 3 ms (33.73%) | Memory: 7.7 MB (17.06%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,18 +1,23 @@
-//dp approach
+//bottom-up dp
+
 class Solution {
 public:
-    int countPaths(int i, int j, int n, int m, vector<vector<int>> &dp) {
-        if (i == (n - 1) && j == (m - 1)) return 1;
-        if (i >= n || j >= m) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        else return dp[i][j] = countPaths(i + 1, j, n, m, dp) + countPaths(i, j + 1, n, m, dp);
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        int num = countPaths(0, 0, m, n, dp);
-        if (m == 1 && n == 1)
-            return num;
-        return dp[0][0];
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) {
+                    dp[i][j] = 1;
+                } else {
+                    if (i > 1) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 1) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
