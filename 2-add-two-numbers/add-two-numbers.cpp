0622377b81e369912eb9fc20440730303e7e180f
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-05-26 19:19:34 +0530

    Time: 30 ms (15.06%) | Memory: 76.2 MB (20.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,36 +11,32 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* newLL = nullptr;
-        ListNode* tail = nullptr;
+        ListNode* dummy = new ListNode{0, nullptr};
+        ListNode* temp = dummy;
         int carry = 0;
 
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+        while (l1 != NULL || l2 != NULL || carry) {
             int sum = carry;
-            if (l1 != nullptr) {
+
+            if (l1 != NULL) {
                 sum += l1->val;
                 l1 = l1->next;
             }
-            if (l2 != nullptr) {
+
+            if (l2 != NULL) {
                 sum += l2->val;
                 l2 = l2->next;
             }
 
-            ListNode* newNode = new ListNode;
-            newNode->val = sum % 10;
-            newNode->next = nullptr;
-
-            if (newLL == nullptr) {
-                newLL = newNode;
-                tail = newNode;
-            } else {
-                tail->next = newNode;
-                tail = newNode;
-            }
-
+            temp->next = new ListNode{sum % 10, nullptr};
             carry = sum / 10;
+            temp = temp->next;
         }
 
-        return newLL;
+        if(carry){
+            temp->next = new ListNode{carry, nullptr};
+        }
+
+        return dummy->next;
     }
 };
