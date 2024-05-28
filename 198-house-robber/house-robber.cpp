d1b3e000be1953d31a7d51ac360bb4e62be86205
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-05-28 16:51:28 +0530

    Time: 2 ms (40.31%) | Memory: 9.2 MB (34.55%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int thief(vector<int>& nums, int idx, vector<int>& dp) {
-        if (idx < 0) {
-            return 0;
-        }
-        if (idx == 0) {
-            return nums[idx];
-        }
-        
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-
-        int pick = nums[idx] + thief(nums, idx - 2, dp);
-        int notPick = 0 + thief(nums, idx - 1, dp);
-
-        return dp[idx] = max(pick, notPick);
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        int ans = thief(nums, n - 1, dp);
+        vector<int> dp(n, 0);
 
-        return ans;
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (i > 1) {
+                dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+            } else {
+                dp[i] = max(dp[i - 1], nums[i]);
+            }
+        }
+
+        return dp[n - 1];
     }
 };
