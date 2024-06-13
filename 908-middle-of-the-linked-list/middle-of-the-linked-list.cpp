59eca3257df2f14e3fca023cba94610a81ea1ba9
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-06-14 00:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.20%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,36 +10,15 @@
  */
 class Solution {
 public:
-    int countLL(ListNode* head) {
-        int ct = 0;
-        ListNode* temp = head;
-        while (temp != NULL) {
-            ct++;
-            temp = temp->next;
-        }
-        return ct;
-    }
-
     ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
 
-        int count = countLL(head);
-        
-
-        if (count % 2 != 0) {
-            int n = (count / 2)+1;
-            ListNode* temp = head;
-            for (int i = 1; i <=n-1; i++) {
-                temp = temp->next;
-            }
-            return temp;
-        } else {
-            int n = (count / 2);
-            ListNode* temp = head;
-            for (int i = 1; i <=n; i++) {
-                temp = temp->next;
-            }
-            return temp;
+        while(fast && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return head;
+
+        return slow;
     }
 };
