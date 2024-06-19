c9625b6c660f94568ea8d6c8b0870eea7b348a20
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-06-19 19:02:12 +0530

    Time: 24 ms (13.98%) | Memory: 20.5 MB (14.83%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+    int helper(int idx, int amount, vector<int>& coins, int n,
+               vector<vector<int>>& dp) {
+        if (amount == 0) {
+            return 1;
+        }
+        if (idx >= n || amount < 0) {
+            return 0;
+        }
+        if (dp[idx][amount] != -1) {
+            return dp[idx][amount];
+        }
+
+        int noTake = helper(idx + 1, amount, coins, n, dp);
+        int take = 0;
+        if (coins[idx] <= amount) {
+            take = helper(idx, amount - coins[idx], coins, n, dp);
+        }
+
+        dp[idx][amount] = take + noTake;
+        return dp[idx][amount];
+    }
+    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
-        
-       
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (j >= coins[i - 1]) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        
-        return dp[n][amount];
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        return helper(0, amount, coins, n, dp);
     }
 };
