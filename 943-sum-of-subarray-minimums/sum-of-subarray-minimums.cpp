646dd8699a28a223a797ab3371322b2e6dc5ea0d
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-06-22 02:52:15 +0530

    Time: 71 ms (36.79%) | Memory: 46.6 MB (20.25%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -7,7 +7,7 @@ public:
         vector<int> left(n), right(n);
         stack<pair<int, int>> s1, s2;
         
-        // Calculate left
+        //left part of the element
         for (int i = 0; i < n; ++i) {
             int count = 1;
             while (!s1.empty() && s1.top().first > arr[i]) {
@@ -18,7 +18,7 @@ public:
             left[i] = count;
         }
         
-        // Calculate right
+        //right part of the element
         for (int i = n - 1; i >= 0; --i) {
             int count = 1;
             while (!s2.empty() && s2.top().first >= arr[i]) {
@@ -29,7 +29,6 @@ public:
             right[i] = count;
         }
         
-        // Calculate the result
         long long result = 0;
         for (int i = 0; i < n; ++i) {
             result = (result + (long long)arr[i] * left[i] * right[i]) % MOD;
