dea8df86018caa9cb1f60c81897be787e864469f
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-05 21:27:21 +0530

    Time: 4 ms (32.39%) | Memory: 14.7 MB (10.51%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -20,21 +20,24 @@ public:
         queue<pair<TreeNode*, int>> q;
         q.push({root, 0});
         while (!q.empty()) {
-            auto it = q.front();
-            int lvl = it.second;
-            TreeNode* node = it.first;
-            q.pop();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto it = q.front();
+                int lvl = it.second;
+                TreeNode* node = it.first;
+                q.pop();
 
-            if (mp.find(lvl) == mp.end()) {
-                mp[lvl] = node->val;
-            }
+                if (mp.find(lvl) == mp.end()) {
+                    mp[lvl] = node->val;
+                }
 
-            if (node->right != NULL) {
-                q.push({node->right, lvl + 1});
-            }
+                if (node->right != NULL) {
+                    q.push({node->right, lvl + 1});
+                }
 
-            if (node->left != NULL) {
-                q.push({node->left, lvl + 1});
+                if (node->left != NULL) {
+                    q.push({node->left, lvl + 1});
+                }
             }
         }
 
