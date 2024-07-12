3caaa39fccca6bcdcc319cca0b0abe8ff12735b8
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-13 00:01:27 +0530

    Time: 10 ms (87.58%) | Memory: 15.2 MB (13.44%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -1,20 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
 public:
     vector<int> postorder(Node* root) {
         vector<int> result;
-        postorderHelper(root, result);
+        if (root == nullptr) {
+            return result;
+        }
+
+        stack<Node*> stk;
+        stk.push(root);
+
+        while (!stk.empty()) {
+            Node* node = stk.top();
+            stk.pop();
+            
+            result.push_back(node->val);
+            
+            for (int i = 0; i < node->children.size(); i++) {
+                stk.push(node->children[i]);
+            }
+        }
+        reverse(result.begin(), result.end());
         return result;
-    }
-
-private:
-    void postorderHelper(Node* node, vector<int>& result) {
-        if (node == nullptr) {
-            return;
-        }
-
-        for (Node* child : node->children) {
-            postorderHelper(child, result);
-        }
-        result.push_back(node->val);
     }
 };
