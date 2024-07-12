166fdb33cf3b398a056ad1d9b62f93cb9d30e0d9
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-13 01:34:52 +0530

    Time: 752 ms (5.21%) | Memory: 54.2 MB (6.53%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    bool helper(vector<int>& nums, int idx, vector<int>& dp, int size) {
-
-        if (idx == size - 1) {
+    bool helper(vector<int>& nums, int idx, vector<int>& dp) {
+        if (idx >= nums.size() - 1) {
             return true;
+        }
+
+        if (nums[idx] == 0) {
+            return false;
         }
 
         if (dp[idx] != -1) {
             return dp[idx];
         }
 
-        if (nums[idx] == 0) {
-            return dp[idx] = false;
-        }
-
-        for (int jump = 1; jump <= nums[idx]; ++jump) {
-
-            if (idx + jump < size && helper(nums, idx + jump, dp, size)) {
+        for (int jump = 1; jump <= nums[idx]; jump++) {
+            if (helper(nums, idx + jump, dp)) {
                 return dp[idx] = true;
             }
         }
@@ -26,8 +24,41 @@ public:
 
     bool canJump(vector<int>& nums) {
         int size = nums.size();
-        vector<int> dp(size, -1);
+        vector<int> dp(size+1, -1);
 
-        return helper(nums, 0, dp, size);
+        return helper(nums, 0, dp);
     }
 };
+
+// giving tle
+//  class Solution {
+//  public:
+//      bool helper(vector<int>& nums, int size, int idx, vector<bool>& dp) {
+//          if (idx >= size) {
+//              return false;
+//          }
+
+//         if (idx == size - 1) {
+//             return true;
+//         }
+
+//         if (dp[idx] != false) {
+//             return dp[idx];
+//         }
+
+//         for (int i = 1; i <= nums[idx]; i++) {
+//             if (helper(nums, size, idx + i, dp)) {
+//                 dp[idx] = true;
+//             }
+//         }
+
+//         return dp[idx];
+//     }
+
+//     bool canJump(vector<int>& nums) {
+//         int size = nums.size();
+//         vector<bool> dp(size + 1, false);
+
+//         return helper(nums, size, 0, dp);
+//     }
+// };
