a31e85eb6e11b3e0f29fd0ce0bbab1e2cfa65d0c
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-13 01:31:27 +0530

    Time: 635 ms (8.48%) | Memory: 54 MB (6.53%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    bool helper(vector<int>& nums, int idx, vector<int>& dp) {
-        int size = nums.size();
-        
+    bool helper(vector<int>& nums, int idx, vector<int>& dp, int size) {
+
         if (idx == size - 1) {
             return true;
         }
-        
-        
+
         if (dp[idx] != -1) {
             return dp[idx];
         }
-        
-        
+
         if (nums[idx] == 0) {
             return dp[idx] = false;
         }
-        
-       
+
         for (int jump = 1; jump <= nums[idx]; ++jump) {
-           
-            if (idx + jump < size && helper(nums, idx + jump, dp)) {
-                return dp[idx] = true; 
+
+            if (idx + jump < size && helper(nums, idx + jump, dp, size)) {
+                return dp[idx] = true;
             }
         }
-        
-        return dp[idx] = false; 
+
+        return dp[idx] = false;
     }
 
     bool canJump(vector<int>& nums) {
         int size = nums.size();
-        vector<int> dp(size, -1); 
-        
-        return helper(nums, 0, dp); 
+        vector<int> dp(size, -1);
+
+        return helper(nums, 0, dp, size);
     }
 };
