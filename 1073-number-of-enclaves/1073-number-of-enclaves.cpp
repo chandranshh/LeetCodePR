f44267354a2185e73a1b258786fed956a1e0bab1
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-29 19:02:23 +0530

    Time: 54 ms (42.69%), Space: 27.9 MB (96.56%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -3,12 +3,35 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if ((i == 0 || i == n - 1 || j == 0 || j == m - 1) &&
-                    grid[i][j] == 1) {
-                    dfs(grid, i, j, n, m);
+                if ((i == 0 || i == n - 1 || j == 0 ||
+                    j == m - 1) && grid[i][j] == 1) {
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                grid[x][y] = 2;
+
+                for (const auto& dir : dirs) {
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
+                        grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                    }
                 }
             }
         }
@@ -23,18 +46,5 @@ public:
             }
         }
         return cnt;
-    }
-
-private:
-    void dfs(vector<vector<int>>& grid, int i, int j, int n, int m) {
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != 1) {
-            return;
-        }
-
-        grid[i][j] = 2;
-        dfs(grid, i + 1, j, n, m);
-        dfs(grid, i - 1, j, n, m);
-        dfs(grid, i, j + 1, n, m);
-        dfs(grid, i, j - 1, n, m);
     }
 };
