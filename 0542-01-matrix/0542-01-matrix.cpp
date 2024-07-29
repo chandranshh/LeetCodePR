648daeeb806c4ea205d6ced33906fa60a8253a0f
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-29 16:44:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
-        if (n == 0) return mat;
+        if (n == 0)
+            return mat;
         int m = mat[0].size();
 
-        vector<vector<int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        queue<pair<pair<int, int>, int>> q; // <coordinates, steps>
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
-                } else {
-                    mat[i][j] = INT_MAX; 
-                }
-            }
-        }
-
-        // BFS to update distances
-        while (!q.empty()) {
-            int x = q.front().first.first;
-            int y = q.front().first.second;
-            int steps = q.front().second;
-            q.pop();
-
-            for (const auto& dir : dirs) {
-                int nx = x + dir[0];
-                int ny = y + dir[1];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && mat[nx][ny] == INT_MAX) {
-                    mat[nx][ny] = steps + 1; 
-                    q.push({{nx, ny}, steps + 1});
+                if (mat[i][j] != 0) {
+                    mat[i][j] = INT_MAX;
+                    dfs(mat, i, j, n, m);
                 }
             }
         }
         return mat;
+    }
+
+private:
+    int dfs(vector<vector<int>>& mat, int i, int j, int n, int m) {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return INT_MAX;
+
+        if (mat[i][j] == INT_MAX) {
+            mat[i][j] =
+                min({dfs(mat, i + 1, j, n, m), dfs(mat, i - 1, j, n, m),
+                     dfs(mat, i, j + 1, n, m), dfs(mat, i, j - 1, n, m)}) +
+                1;
+        }
+        return mat[i][j];
     }
 };
