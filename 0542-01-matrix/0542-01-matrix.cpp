c11ed425f330b30d70b325b7d22ca4253f61c46e
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-07-29 15:46:27 +0530

    Time: 51 ms (71.91%), Space: 35.3 MB (71.82%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,45 +2,38 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
+        if (n == 0) return mat;
         int m = mat[0].size();
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> dist(n, vector<int>(m, 0));
-
         vector<vector<int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-
-        queue<pair<pair<int, int>, int>> q; //<coordinates, steps>
-
+        queue<pair<pair<int, int>, int>> q; // <coordinates, steps>
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
                     q.push({{i, j}, 0});
-                    vis[i][j] = 1;
+                } else {
+                    mat[i][j] = INT_MAX; 
                 }
             }
         }
 
+        // BFS to update distances
         while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                int x = q.front().first.first;
-                int y = q.front().first.second;
-                int steps = q.front().second;
-                q.pop();
-                dist[x][y] = steps;
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
 
-                for (const auto& dir : dirs) {
-                    int nx = x + dir[0];
-                    int ny = y + dir[1];
+            for (const auto& dir : dirs) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
 
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
-                        vis[nx][ny] == 0) {
-                        vis[nx][ny] = 1;
-                        q.push({{nx, ny}, steps + 1});
-                    }
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && mat[nx][ny] == INT_MAX) {
+                    mat[nx][ny] = steps + 1; 
+                    q.push({{nx, ny}, steps + 1});
                 }
             }
         }
-        return dist;
+        return mat;
     }
-}; 
+};
