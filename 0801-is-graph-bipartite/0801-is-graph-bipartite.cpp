f76561a77834bb2ba151cbe449fbf6f3e886d1ed
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-01 17:32:50 +0530

    Time: 11 ms (96.99%), Space: 16.3 MB (63.47%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,60 +1,33 @@
 class Solution {
+private:
+    bool dfs(int node, int col, vector<int>& color,
+             const vector<vector<int>>& graph) {
+        color[node] = col;
+
+        for (const auto& neighbour : graph[node]) {
+            if (color[neighbour] == color[node]) {
+                return false;
+            } else if (color[neighbour] == -1) {
+                if (dfs(neighbour, !col, color, graph) == false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
         vector<int> color(V, -1);
+
         for (int i = 0; i < V; i++) {
             if (color[i] == -1) {
-                queue<int> q;
-                color[i] = 0;
-                q.push(i);
-
-                while (!q.empty()) {
-                    int size = q.size();
-                    while (size--) {
-                        int node = q.front();
-                        q.pop();
-
-                        for (const auto& neighbour : graph[node]) {
-                            if (color[neighbour] == color[node]) {
-                                return false;
-                            } else if (color[neighbour] == -1) {
-                                color[neighbour] = !color[node];
-                                q.push(neighbour);
-                            }
-                        }
-                    }
+                if (dfs(i, 0, color, graph) == false) {
+                    return false;
                 }
             }
         }
         return true;
     }
 };
-
-/*
-int n = graph.size();
-        vector<int> color(n, -1); // -1: uncolored, 0: color 1, 1: color 2
-
-        for (int start = 0; start < n; ++start) {
-            if (color[start] == -1) {
-                queue<int> q;
-                q.push(start);
-                color[start] = 0;
-
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-                    for (int neighbor : graph[node]) {
-                        if (color[neighbor] == -1) {
-                            color[neighbor] = 1 - color[node];
-                            q.push(neighbor);
-                        } else if (color[neighbor] == color[node]) {
-                            return false;
-                            // Conflict, not bipartite
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-*/
