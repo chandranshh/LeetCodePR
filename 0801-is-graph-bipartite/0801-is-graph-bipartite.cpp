efbf5c3090297c564a496d0c7c3e6ac50e338397
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-17 16:05:11 +0530

    Time: 19 ms (46.68%), Space: 16.7 MB (22.7%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,14 +1,12 @@
 class Solution {
-private:
-    bool dfs(int node, int col, vector<int>& color,
-             const vector<vector<int>>& graph) {
-        color[node] = col;
-
-        for (const auto& neighbour : graph[node]) {
-            if (color[neighbour] == color[node]) {
-                return false;
-            } else if (color[neighbour] == -1) {
-                if (dfs(neighbour, !col, color, graph) == false) {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> color(V, -1);
+        
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (!bfs(graph, color, i)) {
                     return false;
                 }
             }
@@ -16,15 +14,22 @@ private:
         return true;
     }
 
-public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> color(V, -1);
+private:
+    bool bfs(const vector<vector<int>>& graph, vector<int>& color, int start) {
+        queue<int> q;
+        color[start] = 0;
+        q.push(start);
 
-        for (int i = 0; i < V; i++) {
-            if (color[i] == -1) {
-                if (dfs(i, 0, color, graph) == false) {
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (const auto& neighbour : graph[node]) {
+                if (color[neighbour] == color[node]) {
                     return false;
+                } else if (color[neighbour] == -1) {
+                    color[neighbour] = !color[node];
+                    q.push(neighbour);
                 }
             }
         }
