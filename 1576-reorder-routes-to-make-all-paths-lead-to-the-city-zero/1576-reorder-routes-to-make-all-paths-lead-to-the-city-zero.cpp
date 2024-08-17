698d5775932d0b8549c3db9c61879d45b1b20a13
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-18 02:36:14 +0530

    Time: 255 ms (74.87%), Space: 114.9 MB (58.62%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,40 +1,25 @@
 class Solution {
+    int dfs(int node, int parent, const vector<vector<pair<int, int>>>& graph) {
+        int ans = 0;
+        
+        for (auto &neighbor : graph[node]) {
+            int x = neighbor.first;
+            int cost = neighbor.second;
+            if (x != parent)
+                ans += dfs(x, node, graph) + cost;
+        }
+        
+        return ans;
+    }
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> graph(n);
-
-        for (auto& connection : connections) {
-            int u = connection[0];
-            int v = connection[1];
-            graph[u].push_back({v, 1});
-            graph[v].push_back({u, 0});
+        
+        for (auto x : connections) {
+            graph[x[0]].push_back({x[1], 1}); 
+            graph[x[1]].push_back({x[0], 0}); 
         }
-
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        vector<bool> vis(n, false);
-        int ans = 0;
-
-        while (!q.empty()) {
-            auto current = q.front();
-            q.pop();
-
-            int node = current.first;
-            int reversals = current.second;
-
-            vis[node] = true;
-            ans += reversals;
-
-            for (auto& neighbor : graph[node]) {
-                int neighborNode = neighbor.first;
-                int neighborReversals = neighbor.second;
-
-                if (!vis[neighborNode]) {
-                    q.push({neighborNode, neighborReversals});
-                }
-            }
-        }
-
-        return ans;
+        
+        return dfs(0, -1, graph);
     }
 };
