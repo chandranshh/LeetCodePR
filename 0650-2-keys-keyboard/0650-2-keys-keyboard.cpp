07e0a6639fc297b19c93e1d8462be3f9a63fd7de
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-19 21:54:50 +0530

    Time: 27 ms (18.08%), Space: 157.1 MB (10.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int helper(int n, bool isCopy, bool isPaste, int st, int copied, vector<vector<int>>& dp) {
+    int helper(int n, int st, int copied, vector<vector<int>>& dp) {
         if (st == n) {
             return 0;
         }
-        if (n <= 0 || st > n) {
+        if (st > n) {
             return 1001; 
         }
         if (dp[st][copied] != -1) {
             return dp[st][copied];
         }
 
-        int copy = INT_MAX, paste = INT_MAX;
-        if (!isCopy && st != copied) {
-            copy = 1 + helper(n, true, false, st, st, dp);
+        int copy = 1001, paste = 1001;
+        if (st != copied) {
+            copy = 1 + helper(n, st, st, dp);
         }
         if (copied > 0) {
-            paste = 1 + helper(n, false, true, st + copied, copied, dp);
+            paste = 1 + helper(n, st + copied, copied, dp);
         }
 
         dp[st][copied] = min(copy, paste);
@@ -29,6 +29,6 @@ public:
         }
 
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return helper(n, false, false, 1, 0, dp);
+        return helper(n, 1, 0, dp);
     }
 };
