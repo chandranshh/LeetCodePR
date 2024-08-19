fc6fb8c44fe6d0dd72185020b412d69ff6ed1062
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-19 21:39:51 +0530

    Time: 16 ms (34.06%), Space: 157.1 MB (9.34%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int helper(int n, bool isCopy, bool isPaste, int st, int copied, vector<vector<int>>& memo) {
+    int helper(int n, bool isCopy, bool isPaste, int st, int copied, vector<vector<int>>& dp) {
         if (st == n) {
             return 0;
         }
         if (n <= 0 || st > n) {
             return 1001; 
         }
-        if (memo[st][copied] != -1) {
-            return memo[st][copied];
+        if (dp[st][copied] != -1) {
+            return dp[st][copied];
         }
 
         int copy = INT_MAX, paste = INT_MAX;
         if (!isCopy && st != copied) {
-            copy = 1 + helper(n, true, false, st, st, memo);
+            copy = 1 + helper(n, true, false, st, st, dp);
         }
         if (copied > 0) {
-            paste = 1 + helper(n, false, true, st + copied, copied, memo);
+            paste = 1 + helper(n, false, true, st + copied, copied, dp);
         }
 
-        memo[st][copied] = min(copy, paste);
-        return memo[st][copied];
+        dp[st][copied] = min(copy, paste);
+        return dp[st][copied];
     }
 
     int minSteps(int n) {
@@ -28,7 +28,7 @@ public:
             return 0;
         }
 
-        vector<vector<int>> memo(n + 1, vector<int>(n + 1, -1));
-        return helper(n, false, false, 1, 0, memo);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return helper(n, false, false, 1, 0, dp);
     }
 };
