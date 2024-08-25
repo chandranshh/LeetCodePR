80d31f712f7bd8f1af7db4303f2bdfe6103113aa
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-25 23:58:50 +0530

    Time: 108 ms (64.26%), Space: 55.6 MB (77.3%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n == 1)
-            return {0};
-        
+        if (n == 1) return {0};
+
         vector<int> result;
-        vector<int> indegree(n);
-        map<int, vector<int>> adj;
-        
-        for(vector<int> vec:edges) {
-            int u = vec[0];
-            int v = vec[1];
+        vector<int> indegree(n, 0);
+        vector<vector<int>> adj(n);
+
+        for (const auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
             indegree[u]++;
             indegree[v]++;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        
+
         queue<int> que;
-        for(int i = 0; i<n; i++) {
-            if(indegree[i] == 1) 
+        for (int i = 0; i < n; ++i) {
+            if (indegree[i] == 1)
                 que.push(i);
         }
-        
-        while(n > 2) {
+
+        while (n > 2) {
             int size = que.size();
-            n -= size; 
-            
-            while(size--) {
+            n -= size;
+
+            while (size--) {
                 int u = que.front();
                 que.pop();
-                for(int v:adj[u]) {
-                    indegree[v]--;
-                    if(indegree[v] == 1)
+                for (int v : adj[u]) {
+                    if (--indegree[v] == 1)
                         que.push(v);
                 }
             }
         }
-        
-        while(!que.empty()) {
+
+        while (!que.empty()) {
             result.push_back(que.front());
             que.pop();
         }
+
         return result;
     }
 };
