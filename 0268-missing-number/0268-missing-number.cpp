dfa981480449b8a93dba790cd793407b16841718
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-08-31 01:15:52 +0530

    Time: 15 ms (50.68%), Space: 21.4 MB (15.9%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,31 @@
 class Solution {
-public:
-int missingNumber(vector<int>& nums) {
-  int n = nums.size();
-  if(n<1 && n>10e4){
-    return -1;
-  }
-  int m=-1;
-
-  vector<int> h(n+1);
-
-  for(int i=0;i<n;i++){
-    h[nums[i]]++;
-  }
-
-  for(int i=0;i<=n;i++){
-    if(h[i]!=1){
-      m=i;
+private:
+    bool isPresent(vector<int>& nums, int num) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == num) {
+                return true;
+            } else if (nums[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
-  }
-  return m;
-}
 
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = -1;
+        sort(nums.begin(), nums.end());
+        for (int num = 0; num <= n; num++) {
+            if (!isPresent(nums, num)) {
+                ans = num;
+            }
+        }
+
+        return ans;
+    }
 };
