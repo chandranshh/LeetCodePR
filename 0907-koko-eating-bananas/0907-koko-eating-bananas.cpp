d71de922c7bc00a3d26a04c6491e4f580328db44
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-09-02 02:49:13 +0530

    Time: 43 ms (9.12%), Space: 21.8 MB (45.89%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,33 +1,30 @@
 class Solution {
-private:
-    bool canEat(int k, int size, int h, vector<int>& piles) {
-        long long totalTime = 0;
-        for (int i = 0; i < size; i++) {
-            totalTime += ceil((double)(piles[i]) / k);
-        }
-        return totalTime <= h;
-    }
-
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi = INT_MIN;
+        int maxi = *max_element(piles.begin(), piles.end());
         int n = piles.size();
-        for (int i = 0; i < n; i++) {
-            maxi = max(maxi, piles[i]);
-        }
         int start = 1, end = maxi;
         int result = maxi;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (canEat(mid, n, h, piles)) {
-                result = mid; 
-                end = mid - 1; 
+                result = mid;
+                end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
 
         return result;
+    }
+
+private:
+    inline bool canEat(int k, int size, int h, vector<int>& piles) {
+        long totalTime = 0;
+        for (int i = 0; i < size; i++) {
+            totalTime += ceil((double)(piles[i]) / k);
+        }
+        return totalTime <= h;
     }
 };
