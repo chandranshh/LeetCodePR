2c8ed89d44a1839c831fc50ac42a5c395de06e5c
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-09-01 20:01:02 +0530

    Time: 24 ms (49.96%), Space: 22.8 MB (86.63%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         sort(arr.begin(), arr.end());
 
         vector<vector<int>> ans;
-
+        //ans.back() is equivalent to using ans[ans.size() - 1]
         for (int i = 0; i < n; i++) {
             if (ans.empty() || arr[i][0] > ans.back()[1]) {
                 ans.push_back(arr[i]);
