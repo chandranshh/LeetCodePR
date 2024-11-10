6ee70f53e4d188997431da9ae3ddfd597eae9451
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-11-11 01:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (73.95%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-
-    //to find nCr
-    int nCr(int n, int r){
-        long long res = 1;
-        for(int i = 0; i < r ; i++){
-            res = res * (n - i);
-            res = res / (i + 1);
-        }
-        return (int)res;
-    }
-
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i = 1; i <= numRows; i++){
-            vector<int>tempArr;
-            for(int j = 1; j<=i; j++){
-                tempArr.push_back(nCr(i - 1, j - 1));
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
             }
-            ans.push_back(tempArr);
+
+            ans.push_back(row);
         }
-        return ans;    
+
+        return ans;
     }
 };
