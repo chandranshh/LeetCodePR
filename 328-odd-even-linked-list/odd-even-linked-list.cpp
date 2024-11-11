e67a9cc92ad6f7c05ab28f9d88c6fa4f73ab7212
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-11-11 19:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (56.99%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) return head;
-        
+        if (!head || !head->next || !head->next->next) return head;
+
         ListNode *odd = head;
         ListNode *even = head->next;
-        ListNode *even_start = head->next;
-        
-        while(odd->next && even->next){
+        ListNode *evenHead = even;
+
+        while (even && even->next) {
             odd->next = even->next; 
-            even->next = odd->next->next; 
-            odd = odd->next;
-            even = even->next;
+            odd = odd->next;        
+            even->next = odd->next; 
+            even = even->next;      
         }
-        odd->next = even_start;  
-        return head; 
+
+        odd->next = evenHead; 
+        return head;
     }
 };
