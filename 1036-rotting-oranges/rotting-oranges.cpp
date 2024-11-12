68f275c2cecad6e2fae1f667b3d7d5ede3d8f1f9
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-11-13 04:07:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (24.49%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int fresh = 0;
+        int time = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        vector<vector<int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        while (!q.empty()) {
+            int size = q.size();
+            bool changed = false;
+
+            for (int i = 0; i < size; i++) {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+
+                for (const auto& dir : dirs) {
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                        fresh--;
+                        changed = true;
+                    }
+                }
+            }
+
+            if (changed) time++;
+        }
+
+        return fresh == 0 ? time : -1;
+    }
+};
