afccdde62f8b3b569e89bdcd7d7d5ad1f6a70440
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-11-14 01:47:52 +0530

    Time: 69 ms (48.96%), Space: 61.3 MB (46.41%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    bool checkPalindrome(string &s) {
-        int n = s.size();
-        if (n == 1) {
-            return true;
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> path;
+        partitionHelper(0, s, path, res);
+        return res;
+    }
+
+    void partitionHelper(int index, string s, vector<string>& path,
+                         vector<vector<string>>& res) {
+        if (index == s.size()) {
+            res.push_back(path);
+            return;
         }
-        int left = 0, right = n - 1;
-        while (left < right) {
-            if (s[left] != s[right]) {
-                return false;
+        for (int i = index; i < s.size(); ++i) {
+            if (isPalindrome(s, index, i)) {
+                path.push_back(s.substr(index, i - index + 1));
+                partitionHelper(i + 1, s, path, res);
+                path.pop_back();
             }
-            left++;
-            right--;
+        }
+    }
+
+    bool isPalindrome(string s, int start, int end) {
+        while (start <= end) {
+            if (s[start++] != s[end--])
+                return false;
         }
         return true;
-    }
-
-    vector<vector<string>> helper(int idx, int end, string& s, unordered_map<int, vector<vector<string>>>& dp) {
-        if (idx >= end) {
-            return {{}};
-        }
-
-        if (dp.find(idx) != dp.end()) {
-            return dp[idx];
-        }
-
-        vector<vector<string>> result;
-        for (int i = idx; i < end; i++) {
-            string substring = s.substr(idx, i - idx + 1);
-            if (checkPalindrome(substring)) {
-                vector<vector<string>> nextPartitions = helper(i + 1, end, s, dp);
-                for (vector<string>& partition : nextPartitions) {
-                    partition.insert(partition.begin(), substring);
-                    result.push_back(partition);
-                }
-            }
-        }
-
-        dp[idx] = result;
-        return result;
-    }
-
-    vector<vector<string>> partition(string s) {
-        unordered_map<int, vector<vector<string>>> dp;
-        return helper(0, s.size(), s, dp);
     }
 };
