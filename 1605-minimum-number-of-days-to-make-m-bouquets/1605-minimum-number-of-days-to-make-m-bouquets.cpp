90df1ed921150c9728bc44cf57be459b7edcc0f7
Chandransh Srivastava (sec-theta) <91943465+chandranshh@users.noreply.github.com>
2024-11-13 13:28:10 +0530

    Time: 17 ms (88.6%), Space: 69.3 MB (39.87%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     bool canBloom(vector<int>& bloomDay, int day, int m, int k) {
-        int ct = 0, noBq = 0;
+        int bouquets = 0;
+        int flowers = 0;
+        
         for (int i = 0; i < bloomDay.size(); i++) {
             if (bloomDay[i] <= day) {
-                ct++;
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
             } else {
-                noBq += (ct / k);
-                ct = 0;
+                flowers = 0;
             }
         }
-        noBq += (ct / k);
-        return noBq >= m;
+        
+        return bouquets >= m;
     }
+
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long val = m * 1ll * k * 1ll;
+        long long totalFlowersNeeded = m * 1ll * k;
         int n = bloomDay.size();
-        if (val > n) {
+        
+        if (totalFlowersNeeded > n) {
             return -1;
         }
-
-        int mini = INT_MAX, maxi = INT_MIN;
-
-        for (int i = 0; i < n; i++) {
-            maxi = max(maxi, bloomDay[i]);
-            mini = min(mini, bloomDay[i]);
+        
+        int minDay = INT_MAX, maxDay = INT_MIN;
+        
+        for (int day : bloomDay) {
+            minDay = min(minDay, day);
+            maxDay = max(maxDay, day);
         }
-
-        int low = mini, high = maxi;
+        
+        int low = minDay, high = maxDay;
+        int result = high;
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (canBloom(bloomDay, mid, m, k)) {
@@ -36,6 +45,7 @@ public:
                 low = mid + 1;
             }
         }
+        
         return low;
     }
 };
